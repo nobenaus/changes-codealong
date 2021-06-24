20e6b5fb9faf5689f3a9ac6702a0415992446b0c
Nadja Obenaus <nadbis@gmail.com>
2021-06-24 19:52:15 +0200

    transform your data NOW

--- transformations.js
+++ transformations.js
@@ -1,18 +1,36 @@
+import { parse } from 'date-fns';
+
+function parseStripeDateFormat(dateString) {
+  return parse(dateString, 'dd/MM/yyyy HH:mm:ss', new Date());
+}
 // Codealong: Data Sorting and Filtering in Node.js
 export function sortBySubscriptionDate(input) {
-  return input;
+  return input.sort(
+    (a, b) => new Date(a.subscribeDate) - new Date(b.subscribeDate),
+  );
 }
 
 export function sortByFirstName(input) {
-  return input;
+  return input.sort((a, b) => {
+    if (a.firstName < b.firstName) {
+      return -1;
+    }
+    if (a.firstName > b.firstName) {
+      return 1;
+    }
+    return 0;
+  });
 }
 
 export function filterToFirstNameStartingWithB(input) {
-  return input;
+  return input.filter((user) => user['First Name'].startsWith('B'));
 }
 
 export function filterToCreatedAfter2010(input) {
-  return input;
+  return input.filter((user) => {
+    const date = parseStripeDateFormat(user.created_at_date);
+    return date <= new Date('2008-01-01');
+  });
 }
 
 // Codealong: Data Aggregation, Deduplication and Cleansing in Node.js
